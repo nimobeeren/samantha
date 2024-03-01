7b838c4312953a8aeaba496a1cf229e13ba27786
Nimo Beeren <nimo.beeren@iodigital.com>
2024-03-01 14:25:13 +0100

    remove node stuff for now

--- play.ts
+++
@@ -1,9 +0,0 @@
-import playSound from "play-sound";
-
-const player = playSound();
-
-export function play(file: string) {
-  return player.play(file, (error) => {
-    if (error) throw error;
-  });
-}
